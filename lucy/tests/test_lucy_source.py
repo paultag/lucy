1dc99f8e1a69ac8fac2ed174f6927ed48b130505
Paul Tagliamonte <tag@pault.ag>
2013-05-25 23:31:34 -0400

    meh

--- lucy/tests/test_lucy_source.py
+++ lucy/tests/test_lucy_source.py
@@ -1,26 +1,25 @@
 from lucy.models.source import Source
+from lucy.models.user import User
 
 
 def test_basic_source():
     """ Test that source routines works """
+    User(_id='joe', name='', email='', gpg='').save()
 
     p = Source(source='fluxbox',
                 version='1.0',
-                owner=None)
+                owner="joe")
     p.save()
 
     p = Source(source='fluxbox',
                 version='2.0',
-                owner=None)
+                owner="joe")
     p.save()
 
     p = Source(source='frucksbox',
                 version='2.0',
-                owner=None)
+                owner="joe")
     x = p.save()
-
-    assert len(list(Source.get_all_versions("fluxbox"))) == 2
-    assert len(list(Source.get_all_versions("frucksbox"))) == 1
 
     obj = Source.load(x)
     assert obj['version'] == '2.0'
